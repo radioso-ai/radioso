6e57ad7a2aa2e89d6f9d602aa941706cad84e660
Dmitri Borohhov <dmitri@ausalt.com>
2026-06-03 15:53:32 +0300

    feat(conversation-nlp): OpenAI ConversationModelGateway adapter (Phase B) (#572)
    * feat: add conversation nlp openai adapter
    
    * fix(conversation-nlp): harden openai gateway errors

---
+++ packages/conversation-nlp/src/index.ts
@@ -0,0 +1,10 @@
+export { OpenAIConversationModelGateway } from "./openAiConversationModelGateway.js";
+export {
+  type OpenAIChatClient,
+  type OpenAIChatCompletionRequest,
+  type OpenAIChatCompletionResponse,
+  type OpenAIChatMessage,
+  type OpenAIChatUsage,
+  type OpenAIConversationModelGatewayOptions,
+  type OpenAIReasoningEffort,
+} from "./openAiTypes.js";
